36cdaf7bd724d6bd8c0c29798b02b5aac97b3327
ccsd-coder <julien.esposito@cc-sd.edu>
2016-02-24 15:50:04 -0800

    An Example for Example - 02 / 24 / 2016
    An Example for Example - 02 / 24 / 2016

---
+++ 2016/MOD02/PRG205/week3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // My 1st object (the soup) of the class (recipe) A
+            A a = new A(
+        }
+    }
+}
